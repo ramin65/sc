886c91f027d7fdc3097c471a9cabfaf4f75a364f
raminub <ramin65@users.noreply.github.com>
2016-10-09 09:50:01 +0330

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,14 @@
+do local _ = {
+  disabled_channels = {},
+  enabled_plugins = {
+    "COREUB"
+  },
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    148617896
+  }
+}
+return _
+end
