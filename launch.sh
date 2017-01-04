3b1aa3ce7b0cdb9151e307f4955f2f9f224d833c
raminub <ramin65@users.noreply.github.com>
2017-01-04 22:02:23 +0330

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -42,5 +42,5 @@ else
     exit 1
   fi
   rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot.lua -l 1 -E $@
 fi
